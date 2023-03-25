cf42f19fc84f39787e30508badfbf4f37ea5fb92
Magn-Broder <magn3442@outlook.dk>
2023-03-25 19:21:54 +0100

    flere ændringer
    flere ændringer

--- ESP32 Nr.1/main.py
+++ ESP32 Nr.1/main.py
@@ -1,21 +1,41 @@
+from umqtt.simple import MQTTClient
+from machine import ADC, Pin
 from time import sleep
-from machine import ADC, Pin, SPI
+import _thread as thread
 import neopixel
+import network
 
-# Variabler til SPI-forbindelse
-spi = SPI(1, baudrate=10000000, polarity=0, phase=0)  # initialize SPI bus
-cs = Pin(5, Pin.OUT)  # set chip select pin
-cs.on() 
+# Mqtt variabler 
+SERVER = 'raspberrypi.local'  # MQTT Server Addresse
+CLIENT_ID = 'ESP32_MQ135_Sensor'
+TOPIC = b'CO2_ppm'
+client = MQTTClient(CLIENT_ID, SERVER)
 
-# Set up the MQ135 sensor
+# MQ135 sensor variabler
 RL_VALUE = 10
 RO_CLEAN_AIR_FACTOR = 9.83
 
-# Variabler til neopixel ring
+# Neopixel variabler
 n = 12
 p = 15
 np = neopixel.NeoPixel(Pin(p), n)
 
+# Instans af ADC klassen
+adc = ADC(Pin(36))
+adc.atten(ADC.ATTN_11DB)
+
+# Funktion til WIFI
+def do_connect(): 
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect('ssid','passphrase')
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+
+# Neopixel funktioner
 def np_off():
     for i in range(12):
         np[i] = (0, 0, 0)
@@ -35,36 +55,49 @@ def np_Red():
     for i in range(12):
         np[i] = (50, 0, 0)
         np.write()
-        
-adc = ADC(Pin(36))
-adc.atten(ADC.ATTN_11DB)
 
+# Funktion til at læse MQ153 sensor
 def read_mq():
     rs = adc.read() / 4095 * 3.3 / (5.0 - adc.read() / 4095 * 3.3) * RL_VALUE
     ratio = rs / RO_CLEAN_AIR_FACTOR
-    co2_ppm = round(2.9820682 * pow(ratio, -1.769034857))
+    co2_ppm = round(1.8
+                    * pow(ratio, -1.769034857))
     return co2_ppm
 
+# Funktion til tråd 1
+def sensor_indikator():
+    while True:
+        co2_ppm = read_mq()
+        print(f"CO2 Level: {co2_ppm} ppm")
+        sleep(1)
+        
+        if co2_ppm <= 400:
+            np_Green()
+            
+        elif co2_ppm <= 600:
+            np_Yellow()
+        
+        else:
+            np_Red()
+
+# Funktion til tråd 2 
+def mqtt_publish_til_RPI():
+    while True:
+        co2_ppm = read_mq()
+        msg = (b'{}'.format(co2_ppm))
+        client.publish(TOPIC, msg)  # Publish sensor data to MQTT topic
+        print(msg)
+        sleep(300)
+
+# Opret forbindelse til WIFI
+do_connect()
+
+# Opret forbindelse til MQTT broker
+client.connect()   
+
+# Sluk Neopixel
 np_off()
 
-while True:
-    co2_ppm = read_mq()
-    print(f"CO2 Level: {co2_ppm} ppm")
-    sleep(1)
-    
-    
-    value = 42  # integer to send
-    cs.off()  # select chip
-    spi.write(bytearray([value]))  # send integer as a single byte
-    cs.on()  # deselect chip
-    sleep(1)  # wait for 1 second before sending another integer
-    
-    if co2_ppm <= 400:
-        np_Green()
-        
-    elif co2_ppm <= 600:
-        np_Yellow()
-    
-    else:
-        np_Red()
-
+# Start 2 nye tråde
+thread.start_new_thread(sensor_indikator,())
+thread.start_new_thread(mqtt_publish_til_RPI,())
