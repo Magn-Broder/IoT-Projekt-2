2519fe75b818e7fd1ed487bb67e0c0bd1de6420c
Magn-Broder <magn3442@outlook.dk>
2023-03-27 11:15:22 +0200

    hekka

---
+++ Raspberry PI 3A+/MQ135_web/MQ135_web.py
@@ -0,0 +1,90 @@
+#!/home/magn3442/Projects/IoT2 python
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import io
+
+from flask import Flask, render_template, send_file, make_response, request
+app = Flask(__name__)
+
+import sqlite3
+conn=sqlite3.connect('../sensor_data.db', check_same_thread=False)
+curs=conn.cursor()
+
+# Modtag den nyeste data fra databasen 
+def get_last_data():
+	for row in curs.execute("SELECT * FROM MQ135_data ORDER BY timestamp DESC LIMIT 1"):
+		time = row[0]
+		ppm = row[1]
+		print(time)
+	#conn.close()
+	return time, ppm
+
+def get_hist_data (num_samples):
+	curs.execute("SELECT * FROM MQ135_data ORDER BY timestamp DESC LIMIT "+str(num_samples))
+	data = curs.fetchall()
+	dates = []
+	ppm = []
+
+	for row in reversed(data):
+		dates.append(row[0])
+		ppm.append(row[1])
+	
+	return dates, ppm
+
+def max_rows_table():
+	for row in curs.execute("select COUNT(ppm) from  MQ135_data"):
+		max_number_rows=row[0]
+	return max_number_rows
+
+# define and initialize global variables
+global num_samples
+num_samples = max_rows_table()
+if (num_samples > 101):
+	num_samples = 100
+
+# main route
+@app.route("/")
+def index():
+	time, ppm = get_last_data()
+	templateData = {
+	  	'time'  : time,
+		'ppm'   : ppm,		
+      	'num_samples'	: num_samples
+	}
+	return render_template('index.html', **templateData)
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    global num_samples
+    num_samples = int (request.form['num_samples'])
+    num_max_samples = max_rows_table()
+    if (num_samples > num_max_samples):
+        num_samples = (num_max_samples-1)
+    time, ppm = get_last_data()
+    templateData = {
+	  	'time'  : time,
+      	'ppm'   : ppm,
+      	'num_samples'	: num_samples
+	}
+    return render_template('index.html', **templateData)
+
+@app.route('/plot/ppm')
+def plot_ppm():
+	times, ppms = get_hist_data(num_samples)
+	ys = ppms
+	fig = Figure()
+	axis = fig.add_subplot(1, 1, 1)
+	axis.set_title("CO2 [PPM]")
+	axis.set_xlabel("Samples")
+	axis.grid(True)
+	xs = range(num_samples)
+	axis.plot(xs, ys)
+	canvas = FigureCanvas(fig)
+	output = io.BytesIO()
+	canvas.print_png(output)
+	response = make_response(output.getvalue())
+	response.mimetype = 'image/png'
+	return response
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=80, debug=False)
---
+++ Raspberry PI 3A+/log_MQ135.py
@@ -0,0 +1,38 @@
+#!/home/magn3442/Projects/IoT2 python
+import paho.mqtt.client as mqtt
+import sqlite3
+import time
+
+db_name = 'sensor_data.db'
+
+# log sensor data on database
+def log_data (ppms):
+
+	conn=sqlite3.connect(db_name)
+	curs=conn.cursor()
+	curs.execute("INSERT INTO MQ135_data values(datetime('now'), (?))", (ppms,))
+	conn.commit()
+	conn.close()
+
+def on_connect(client, userdata, flags, rc):
+    print('Connected with result code {0}'.format(rc))
+    # Subscribe (or renew if reconnect).
+    client.subscribe('CO2_ppm')
+
+# Callback fires when a published message is received.
+def on_message(client, userdata, msg):
+	# Decode temperature and humidity values from binary message paylod.
+	ppm = [float(x) for x in msg.payload.decode("utf-8").split(',')]
+	# print('{0} ppm'.format(ppm))
+	ppms = ppm[0]
+	ppm_int = int(ppms)
+	# print(ppm_int)
+	log_data(ppm_int)
+    
+client = mqtt.Client()
+client.on_connect = on_connect  # Specify on_connect callback
+client.on_message = on_message  # Specify on_message callback
+client.connect('localhost', 1883, 60)  # Connect to MQTT broker (also running on Pi).
+
+# Processes MQTT network traffic, callbacks and reconnections. (Blocking)
+client.loop_forever()
