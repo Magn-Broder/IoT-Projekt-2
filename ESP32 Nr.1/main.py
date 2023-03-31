760fa5483ef79556d3a3501c22c0a573f3c05f15
Magn-Broder <89405705+Magn-Broder@users.noreply.github.com>
2023-03-31 10:33:25 +0200

    Update main.py

--- ESP32 Nr.1/main.py
+++ ESP32 Nr.1/main.py
@@ -1,9 +1,9 @@
 from umqtt.simple import MQTTClient
-from machine import ADC, Pin
-from time import sleep
+from machine import I2C, Pin
 import _thread as thread
+from time import sleep
 import neopixel
-import network
+
 
 # Mqtt variabler 
 SERVER = 'raspberrypi.local'  # MQTT Server Addresse
@@ -11,18 +11,10 @@ CLIENT_ID = 'ESP32_MQ135_Sensor'
 TOPIC = b'CO2_ppm'
 client = MQTTClient(CLIENT_ID, SERVER)
 
-# MQ135 sensor variabler
-RL_VALUE = 10
-RO_CLEAN_AIR_FACTOR = 9.83
-
 # Neopixel variabler
 n = 12
 p = 15
 np = neopixel.NeoPixel(Pin(p), n)
-
-# Instans af ADC klassen
-adc = ADC(Pin(36))
-adc.atten(ADC.ATTN_11DB)
 
 # Neopixel funktioner
 def np_off():
@@ -45,38 +37,55 @@ def np_Red():
         np[i] = (50, 0, 0)
         np.write()
 
-# Funktion til at læse MQ153 sensor
-def read_mq():
-    rs = adc.read() / 4095 * 3.3 / (5.0 - adc.read() / 4095 * 3.3) * RL_VALUE
-    ratio = rs / RO_CLEAN_AIR_FACTOR
-    co2_ppm = round(1.8 * pow(ratio, -1.769034857))
-    return co2_ppm
+# Initialize I2C bus
+i2c = I2C(scl=Pin(22), sda=Pin(21), freq=400000)
+address = 0x49
 
-# Funktion til tråd 1
+# Function to read ADC data from MCP3021
+def read_adc():
+    data = bytearray(2)
+    i2c.writeto(address, bytes([0x68]))
+    i2c.readfrom_into(address, data)
+    adc = ((data[0] & 0x0F) << 8) | data[1]
+    return adc
+
+# Function to convert ADC data to CO2 concentration in ppm
+def adc_to_ppm(adc):
+    ADC_0 = 300  # Replace with your sensor's values
+    ADC_400 = 1700
+    ADC_10000 = 4095
+    ppm_400 = 400
+    ppm_10000 = 10000
+    ppm = round((adc - ADC_0) * (ppm_10000 - ppm_400) / (ADC_10000 - ADC_400) + ppm_400)
+    return ppm
+
+# Funktion til tråd 1   
 def sensor_indikator():
     while True:
-        co2_ppm = read_mq()
+        adc_value = read_adc()
+        co2_ppm = adc_to_ppm(adc_value)
         print(f"CO2 Level: {co2_ppm} ppm")
         sleep(1)
-        
-        if co2_ppm <= 600:
-            np_Green()
             
+        if co2_ppm <= 500:
+            np_Green()
+                
         elif co2_ppm <= 800:
             np_Yellow()
-        
+            
         else:
             np_Red()
-
-# Funktion til tråd 2 
+            
+# Funktion til tråd 2        
 def mqtt_publish_til_RPI():
     while True:
-        co2_ppm = read_mq()
+        adc_value = read_adc()
+        co2_ppm = adc_to_ppm(adc_value)
         msg = (b'{}'.format(co2_ppm))
         client.publish(TOPIC, msg)  # Publish sensor data to MQTT topic
         print(msg)
-        sleep(300)
-
+        sleep(30)
+        
 # Opret forbindelse til MQTT broker
 client.connect()   
 
